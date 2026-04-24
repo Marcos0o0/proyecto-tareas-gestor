87d196263bbf239eed226fdefcb0374712e7f548
Marcos0o0 <marcos.godoy00001@gmail.com>
2026-04-24 19:38:07 -0400

    Agrego formato responsive en 3 tamaños

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,36 +3,51 @@ document.getElementById('formulariotareas').addEventListener('submit', function(
     const description = document.getElementById('descripcion').value;
     const dueDate = document.getElementById('fecha').value;
     const prioridad = document.getElementById('prioridad').value;
-    const listaTarea = document.getElementById('taskList');
 
     const itemTarea = document.createElement('li');
     itemTarea.innerHTML = `<div class="tarea-pendiente"><strong>${description}</strong> - Vence el ${dueDate} - Prioridad: ${prioridad} <div class="btn-container">
-            <button class="btn-mover" id="mover-btn-progreso">Mover a En Progreso</button> 
-            <button class="btn-eliminar" id="eliminar-tarea">Eliminar tarea</button>
-        </div>`;
+            <button class="btn-mover">Mover a En Progreso</button> 
+            <button class="btn-eliminar">Eliminar tarea</button>
+        </div></div>`;
     document.getElementById('pendientes-container').appendChild(itemTarea);
     document.getElementById('c-pendiente').textContent = parseInt(document.getElementById('c-pendiente').textContent) + 1;
     this.reset();
 
-    itemTarea.querySelector('#eliminar-tarea').addEventListener('click', function() {
+    itemTarea.querySelector('.btn-eliminar').addEventListener('click', function() {
         itemTarea.remove();
         document.getElementById('c-pendiente').textContent = parseInt(document.getElementById('c-pendiente').textContent) - 1;
     });
 
-    itemTarea.querySelector('#mover-btn-progreso').addEventListener('click', function() {
+    itemTarea.querySelector('.btn-mover').addEventListener('click', function() {
         const tareaEnProgreso = document.createElement('li');
-        tareaEnProgreso.innerHTML = `<div class="tarea-progreso">${description} - Vence el ${dueDate} - Prioridad: ${prioridad} <button class="btn-mover" id="mover-btn-completada">Mover a Completada</button> </div>`;
+        tareaEnProgreso.innerHTML = `<div class="tarea-progreso">${description} - Vence el ${dueDate} - Prioridad: ${prioridad} <div class="btn-container">
+                <button class="btn-mover">Mover a Completada</button>
+                <button class="btn-eliminar">Eliminar tarea</button>
+            </div></div>`;
         document.getElementById('en-progreso-container').appendChild(tareaEnProgreso);
         document.getElementById('c-pendiente').textContent = parseInt(document.getElementById('c-pendiente').textContent) - 1;
         document.getElementById('c-progreso').textContent = parseInt(document.getElementById('c-progreso').textContent) + 1;
         itemTarea.remove();
-        tareaEnProgreso.querySelector('#mover-btn-completada').addEventListener('click', function() {
+
+        tareaEnProgreso.querySelector('.btn-eliminar').addEventListener('click', function() {
+            tareaEnProgreso.remove();
+            document.getElementById('c-progreso').textContent = parseInt(document.getElementById('c-progreso').textContent) - 1;
+        });
+
+        tareaEnProgreso.querySelector('.btn-mover').addEventListener('click', function() {
             const tareaCompletada = document.createElement('li');
-            tareaCompletada.innerHTML = `<div class="tarea-completado">${description} - Vence el ${dueDate} - Prioridad: ${prioridad}</div>`;
+            tareaCompletada.innerHTML = `<div class="tarea-completado">${description} - Vence el ${dueDate} - Prioridad: ${prioridad} <div class="btn-container">
+                    <button class="btn-eliminar">Eliminar tarea</button>
+                </div></div>`;
             document.getElementById('completadas-container').appendChild(tareaCompletada);
             document.getElementById('c-progreso').textContent = parseInt(document.getElementById('c-progreso').textContent) - 1;
             document.getElementById('c-completadas').textContent = parseInt(document.getElementById('c-completadas').textContent) + 1;
             tareaEnProgreso.remove();
+
+            tareaCompletada.querySelector('.btn-eliminar').addEventListener('click', function() {
+                tareaCompletada.remove();
+                document.getElementById('c-completadas').textContent = parseInt(document.getElementById('c-completadas').textContent) - 1;
+            });
         });
     });
 });
---
+++ docs/uso_ia.md
@@ -0,0 +1,9 @@
+Uso de Copilot para autocompletar algunas lineas dentro de CSS
+
+Sin uso de IA General, sin prompt que comentar
+
+Fuentes
+- https://www.w3schools.com/html/default.asp
+- https://www.w3schools.com/css/default.asp
+- https://www.w3schools.com/js/default.asp
+- https://medium.com/@thianlopezz/construyo-un-kanban-board-en-vanilla-javascript-react-angular-y-flutter-parte-1-85b235fe526c
